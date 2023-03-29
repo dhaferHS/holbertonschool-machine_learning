e97fcc7318a081e45a3d30c794c9c8a22b0b71ab
dhaferHS <dhafer.darkprod@gmail.com>
2023-03-29 12:54:00 +0200

    Add 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,9 +3,9 @@
 
 """a fonction tha calculate the sape of a matrix and return itt """
 
+
 def matrix_shape(matrix):
     """matrix_shape = fonction to calculate the sape of matrix"""
-
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
