d96e18e295d33c451f96a0b3a144d0fe7bb2dcc1
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-10 23:56:55 +0200

    9-sum_total.py

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" a function thta calculates the summ of a random number"""
+
+def summation_i_squared(n):
+    """ summation_i_squared = calculate the squared of a summ """
+    if not isinstance(n, int) or n < 1:
+        return None
+    return sum(i**2 for i in range(1, n+1)) 
