0f9a4fa61c40d6ad99d53d852d71e6457ce37a10
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-11 00:23:52 +0200

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,6 +2,7 @@
 
 """ a function thta calculates the summ of a random number"""
 
+
 def summation_i_squared(n):
     """ summation_i_squared = calculate the squared of a summ """
     if isinstance(n, int) and n >= 1:
