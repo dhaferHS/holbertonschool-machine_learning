9e4a7c2dc9f780426f864a18b4d01d1ec25c0be2
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-11 00:17:46 +0200

    9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """ summation_i_squared = calculate the squared of a summ """
-    if not isinstance(n, int) or n < 1:
+    while not isinstance(n, int) or n < 1:
         return None
     return sum(i**2 for i in range(1, n+1)) 
