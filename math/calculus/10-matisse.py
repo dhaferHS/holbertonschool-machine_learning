daddb6b1c6fdbaa95272ec139187198394c8f976
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-11 00:47:34 +0200

    10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -5,7 +5,7 @@
 
 def poly_derivative(poly):
     """ a function that calculates the poly derivation of the calculation"""
-    if not isinstance(poly, list) or not all(isinstance(x, (int, float)) for x in poly):
+    if not isinstance(poly, list) or not poly or len(ploy) == 0:
         return None  # poly is not valid
     if len(poly) <= 1:
         return [0]  # derivative is 0
