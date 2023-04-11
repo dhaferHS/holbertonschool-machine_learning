c09baf2404a11bf79b09080c973d4ed9055f9980
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-11 23:06:25 +0200

    3-flip_me_over.py

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+
+""" a function that transpose a matrix """
+
+
+def matrix_transpose(matrix):
+    """ transpose a matrix """
+    
+    
+    
+    MAT=np.array(matrix)
+    return(MAT.transpose())
