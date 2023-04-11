0d39c6e9518e7efb9abbe82fb8707540107c5950
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-11 23:27:53 +0200

    3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -6,9 +6,8 @@ def matrix_transpose(matrix):
     """ transpose a matrix """
     rows = len(matrix)
     cols = len(matrix[0])
-    
-    transpose = [[matrix[j][i] for j in range(rows)] for i in range(cols)]
-    
 
-    
+    transpose = [[matrix[j][i] for j in range(rows)] for i in range(cols)]
+
+
     return transpose
