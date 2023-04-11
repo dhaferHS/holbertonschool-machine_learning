79c6f2a125bdc9c513463d4cf774cd650a8472f2
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-12 00:27:02 +0200

    4-line_up.py

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" function that add two arrays """
+def add_arrays(arr1, arr2):
+    """ add two arrays"""
+    if len(arr1) != len(arr2):
+        return None
+    add = []
+    for i in range(len(arr1)):
+        add.append(arr1[i] + arr2[i])
+    return add
