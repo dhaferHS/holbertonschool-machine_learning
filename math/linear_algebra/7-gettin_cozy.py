bb6930a55a7ab6643e39527f3a3ce09d17cb634c
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-17 00:53:46 +0200

    Add 7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python3
-"""Concatenates two matrices along a specific axis."""  
+"""conctinate  matrices"""
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
-    
+    """concatenate  matrices"""
+    if axis not in [0, 1]:
+        return None
 
-    """Concatenates two matrices along a specific axis.
-    Arguments:
-    mat1 -- a 2D matrix
-    mat2 -- a 2D matrix
-    axis -- an integer representing the axis along which to concatenate the matrices
-    
-    Returns:
-    A new 2D matrix that is the result of concatenating mat1 and mat2 along the specified axis.
-    """
-    result = []
-    for i in range(len(mat1)):
-        return (result.append(mat1[i] + mat2[i]))
+    if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
 
+        mat = []
+        for i in mat1:
+            mat.append(i.copy())
+        for i in mat2:
+            mat.append(i.copy())
+        return mat
+
+    else:
+        if len(mat1) != len(mat2):
+            return None
+        mat = []
+        for i in range(len(mat1)):
+            row = mat1[i].copy() + mat2[i].copy()
+            mat.append(row)
+
+        return mat
