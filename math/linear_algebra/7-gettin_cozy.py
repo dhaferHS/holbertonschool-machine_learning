8991c6cdc3301507fccfa3e12fe0a3c3eecb096d
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-17 00:15:00 +0200

    Add 7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,8 +1,26 @@
-#!/usr/bin/env python3
-import numpy
-""" a functin that concatenates two matrices and return only one """
-
-
 def cat_matrices2D(mat1, mat2, axis=0):
-    return (numpy.concatenate((mat1, mat2), axis=axis))
+    """
+    Concatenates two matrices along a specific axis.
     
+    Arguments:
+    mat1 -- a 2D matrix
+    mat2 -- a 2D matrix
+    axis -- an integer representing the axis along which to concatenate the matrices
+    
+    Returns:
+    A new 2D matrix that is the result of concatenating mat1 and mat2 along the specified axis.
+    """
+    if axis == 0:
+        result = []
+        for row in mat1:
+            result.append(row)
+        for row in mat2:
+            result.append(row)
+        return result
+    elif axis == 1:
+        result = []
+        for i in range(len(mat1)):
+            result.append(mat1[i] + mat2[i])
+        return result
+    else:
+        raise ValueError("Axis must be either 0 or 1.")
