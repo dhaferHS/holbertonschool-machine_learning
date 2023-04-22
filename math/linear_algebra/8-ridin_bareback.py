3bf9bf598271811685ea2b1dbb5c2527e93c55f0
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-22 15:33:40 +0200

    Add 8-ridin_bareback.py

---
+++ math/linear_algebra/8-ridin_bareback.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+""" multiplacate two matrices """
+
+
+def mat_mul(mat1, mat2):
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    result = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat2[0])):
+            sum = 0
+            for k in range(len(mat2)):
+                sum += mat1[i][k] * mat2[k][j]
+            row.append(sum)
+        result.append(row)
+
+    return result
