606f3fc18ad93f624f91ab93d415d73c669a8d46
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-26 21:25:03 +0200

    Add 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-"""a function that perofrms element wise addition substraction multiplication and division"""
+"""function that perofrms addition substraction multiplication and division"""
 
 
 def np_elementwise(mat1, mat2):
-    """a function that perofrms element wise addition substraction multiplication and division"""
+    """function that perofrms addition substraction multiplication and division"""
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
-
