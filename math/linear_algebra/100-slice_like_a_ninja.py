4c14de4aa38473b2be52c00f850b7e0ecabcb33d
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-27 03:02:15 +0200

    Add 100-slice_like_a_ninja.py

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """slice a matrics along a specific axes"""
-import numpy as np
-
 
 def np_slice(matrix, axes={}):
     """slice a matrics along a specific axes"""
-    slices = [slice(None)] * matrix.ndim  # create a slice for each axis
+    slices = [slice(None)] * len(matrix.shape)  # create a slice for each axis
     for axis, index in axes.items():
-        if axis >= 0 and axis < matrix.ndim:  # check if axis is a valid index
+        if 0 <= axis < len(matrix.shape):  # check if axis is a valid index
             slices[axis] = slice(*index)  # update the slice for the specified axis
-    return matrix[tuple(slices)]  # convert the list of slices to a tuple and slice the matrix
+    # use the tuple of slices to slice the matrix
+    result = matrix[slices[0]]
+    for s in slices[1:]:
+        result = result[s]
+    return result
