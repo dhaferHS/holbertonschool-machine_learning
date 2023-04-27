75204c6b1621e05288f75132c795842ad0707ba7
dhaferHS <dhafer.darkprod@gmail.com>
2023-04-27 02:41:06 +0200

    Add 14-saddle_up.py

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,5 +2,7 @@
 """functon that performs mmatrix multiplication"""
 import numpy as np
 
+
 def np_matmul(mat1, mat2):
+    """functon that performs mmatrix multiplication"""
     return np.matmul(mat1, mat2)
