380bfb024e6789beef7dd7f9dc910164a8523ecf
dhaferHS <dhafer.darkprod@gmail.com>
2023-05-02 23:55:25 +0200

    Add poisson.py

---
+++ math/probability/poisson.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Poisson that represents a poisson distribution of data """
+class Poisson:
+    def __init__(self, data=None, lambtha=1.):
+        """Poisson that represents a poisson distribution of data """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+            
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) <= 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = sum(data) / float(len(data))
