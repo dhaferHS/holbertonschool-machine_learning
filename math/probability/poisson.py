c6c16ca087df6987029f51c29283f6f0db26a921
dhaferHS <dhafer.darkprod@gmail.com>
2023-05-03 14:52:28 +0200

    Add poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -16,6 +16,7 @@ class Poisson:
             if len(data) <= 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = sum(data) / float(len(data))
+
     def pmf(self, k):
         """Calculates the value of the PMF for a given number of “successes”"""
         k = int(k)
