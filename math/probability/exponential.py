c79a5dfaf7b5f651b38f55075dbb933b0725a380
dhaferHS <dhafer.darkprod@gmail.com>
2023-05-03 15:57:44 +0200

    Add exponential.py

---
+++ math/probability/exponential.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python3
+"""Exponential that represents an exponential distribution"""
+
+
+class Exponential:
+    """Exponential distribution class"""
+    def __init__(self, data=None, lambtha=1.):
+        self.lambtha = float(lambtha)
+        if data is None:
+            if lambtha <= 0.0:
+                raise ValueError("lamdtha must be a positive value")
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain at least two values")
+            self.lambtha = 1.0 / (sum(data) / float(len(data)))
