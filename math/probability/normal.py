e95fffb2b2383d15634ea75b56b3d893dcbb98bd
dhaferHS <dhafer.darkprod@gmail.com>
2023-05-10 12:16:53 +0200

    Add normal.py

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -33,5 +33,5 @@ class Normal:
         """calculate the propability density function"""
         pi = 3.1415926536
         e = 2.7182818285
-        
-        return (1 / (self.stddev * (2 * pi) ** 0.5)) * e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
+        return (1 / (self.stddev * (2 * pi) ** 0.5)) *\
+            e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
