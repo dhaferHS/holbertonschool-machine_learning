ba5dbaf9b54952f936d9744b14f56b9fe6b4b63e
dhaferHS <dhafer.darkprod@gmail.com>
2023-06-22 16:41:26 +0100

    task -0-

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -12,6 +12,6 @@ class Neuron:
         elif nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.W = np.random.normal(size=(nx, 1))
+        self.W = np.random.randn(nx).reshape(1, nx)
         self.b = 0
         self.A = 0
