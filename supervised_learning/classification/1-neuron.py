710fbe0b6cb35fab3e9880956598733b15c3773f
dhaferHS <dhafer.darkprod@gmail.com>
2023-06-26 09:53:24 +0100

    task -1-

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""supervised neuron file that recognise numbers using some trained data"""
+import numpy as np
+
+
+class Neuron:
+    """class for supervised neuron"""
+
+    def __init__(self, nx):
+        """initialisation of the supervised neuron"""
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        """public instance"""
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
+        """private instance"""
+
+        self.__w = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
