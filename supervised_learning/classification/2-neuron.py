f3546540d8aebf37c13adf618bad13535fa6713d
dhaferHS <dhafer.darkprod@gmail.com>
2023-06-26 15:56:13 +0100

    task -2-

--- supervised_learning/classification/2-neuron.py
+++ supervised_learning/classification/2-neuron.py
@@ -38,6 +38,8 @@ class Neuron:
 
     def forward_prop(self, X):
         """creating public method for forword propagation"""
-        Z = ( self.__W * X ) + self.__b
+        """calculating the forward propagation"""
+        Z = (self.__W * X) + self.__b
+        """creating and calcualtig the activation function for the neuron system"""
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A
