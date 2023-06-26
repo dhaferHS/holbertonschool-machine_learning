c8f605c2b1e7b476699facaaad89ca24daa1dff2
dhaferHS <dhafer.darkprod@gmail.com>
2023-06-26 09:52:48 +0100

    task -0-

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -18,9 +18,4 @@ class Neuron:
         self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
-        """private instance"""
-
-        self.__w = np.random.randn(1, nx)
-        self.__b = 0
-        self.__A = 0
 
