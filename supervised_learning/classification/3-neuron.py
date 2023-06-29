186438079f42ef704c6a25b81eb9d11fe1476f88
dhaferHS <dhafer.darkprod@gmail.com>
2023-06-29 16:38:21 +0100

    task -3-

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -53,5 +53,5 @@ class Neuron:
 
     def cost(self, Y, A):
         m = Y.shape[1]
-        cost = (1 / m) * sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
