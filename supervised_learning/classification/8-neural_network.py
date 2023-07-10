5e0f4bb29230368b89339878e1aa47c212ba764a
dhaferHS <dhafer.darkprod@gmail.com>
2023-07-10 22:34:31 +0100

    task -8-

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import numpy as np
+
+""" creating a class for the neural network from scratch """
+
+
+class NeuralNetwork:
+    """ initializing everything in the neural network """
+
+    """ a public method iinitialization """
+
+    def __init__(self, nx, nodes):
+        """ nx is the number of inpute data """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nodes must be a positive integer")
+            """ nodes is the number of nodes found in the hiden layer """
+
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        """ the weights vector for the hiden layer"""
+        """ initialisied withe a random normal distribution value"""
+        """ w1 is the connection between the inpute and the hiden layer """
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = 0
+        self.A1 = 0
+        """ w2  is the connection between the hiden layer and the output"""
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
